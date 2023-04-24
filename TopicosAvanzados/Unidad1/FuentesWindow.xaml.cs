02f22405a95de9c3f6e97c10f16a4bf0f3968e3c
Sweetx <memosaul.a@hotmail.com>
2023-04-24 00:08:42 -0600

    Agregar archivos de proyecto.

---
+++ TopicosAvanzados/Unidad1/FuentesWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TopicosAvanzados.Unidad1
+{
+    /// <summary>
+    /// Lógica de interacción para FuentesWindow.xaml
+    /// </summary>
+    public partial class FuentesWindow : Window
+    {
+        public FuentesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
